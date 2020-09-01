450d769250ad7030ce2df70f158ee5ac16eab859
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-09-01 11:51:49 +0100

    deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import TodoBanner from './TodoBanner';
 import TodoCreator from './TodoCreator';
 import TodoRow from './TodoRow';
 import VisibilityControl from './VisibilityControl';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap.css'
 
 class App extends Component {
 
