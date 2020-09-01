966224aff362ce694353b863fde9a17b2da7ed8c
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-09-01 01:46:36 +0100

    final commit

---
+++ build/precache-manifest.2fb2bb0742ab94eedf70ed29d67e81d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1c300da4b2fc466b0920d1afce6e39c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d2f20f3d236c99d8967",
+    "url": "/static/css/2.98f886d0.chunk.css"
+  },
+  {
+    "revision": "4ecab39146c712a18835",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8d2f20f3d236c99d8967",
+    "url": "/static/js/2.fd0d544e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fd0d544e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ecab39146c712a18835",
+    "url": "/static/js/main.30cbc8cd.chunk.js"
+  },
+  {
+    "revision": "ce7b350515357ce83319",
+    "url": "/static/js/runtime-main.b56792ae.js"
+  }
+]);
